0334c4635f887196a4efda362f825bb9cfa7b9cb
Marcin Stachniuk <marcin.stachniuk@contractors.roche.com>
2015-03-06 15:51:37 +0100

    Sort List of numbers with Lambda Expression

--- src/main/java/com/blogspot/mstachniuk/java8examples/ex00/ListSortLambdaExample.java
+++ src/main/java/com/blogspot/mstachniuk/java8examples/ex00/ListSortLambdaExample.java
@@ -1,7 +1,6 @@
 package com.blogspot.mstachniuk.java8examples.ex00;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
 public class ListSortLambdaExample {
@@ -9,12 +8,7 @@ public class ListSortLambdaExample {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 5, 6, 7, 2, 4, 3);
 
-        numbers.sort(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer n1, Integer n2) {
-                return n2 - n1;
-            }
-        });
+        numbers.sort((n1, n2) -> n2 - n1);
 
         System.out.println(numbers);
     }
